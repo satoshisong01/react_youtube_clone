8e1396e9216c50825d1f93fe65a2a32a2caa2afc
Sysner04 <sysner04@Sysner04ui-Macmini.local>
2021-03-09 10:17:26 +0900

    video title

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() =>{ //마운트
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBoGl9WgiNj01LxuDKxfI1E4sMBa1aoqzE", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+  return <VideoList videos={videos}/>;
+}
+
+export default App;
