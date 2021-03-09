07a937cfab9eda0081b7598b9cba9fd2c8d3e38a
Sysner04 <sysner04@Sysner04ui-Macmini.local>
2021-03-09 20:14:02 +0900

    0309 20_13

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 import Youtube from './service/youtube';
+import axios from 'axios';
 
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_KEY_API_KEY);
+const httpClient = axios.create({
+    baseURL: 'https://youtube.googleapis.com/youtube/v3',
+    params: {key: process.env.REACT_APP_YOUTUBE_KEY_API_KEY},
+});
+
+const youtube = new Youtube(httpClient);
+
 ReactDOM.render(
   <React.StrictMode>
     <App youtube={youtube}/>
